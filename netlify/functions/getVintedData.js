e0e2751e3592371f0a56fa715fd71ed7e4d9c456
netraax <dils389@gmail.com>
2024-11-27 20:42:33 +0100

    Update getVintedData.js

--- netlify/functions/getVintedData.js
+++ netlify/functions/getVintedData.js
@@ -1,1 +1,21 @@
-
+exports.handler = async function(event) {
+    try {
+        const vintedUrl = JSON.parse(event.body).url;
+        
+        // Simulation de données pour le moment
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                totalItems: 42,
+                totalSold: 156,
+                totalLikes: 873,
+                avgPrice: "15.99€"
+            })
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Une erreur s'est produite" })
+        }
+    }
+}
